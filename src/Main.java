1251c51809e47c8bd86081643790d93c99b98778
Olga Novikova <novikova.os@gmail.com>
2022-02-20 20:10:34 +0300

    lesson program

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float ticketPrice = 10743;
+        int mileCost = 20;
+        int miles = (int)ticketPrice/mileCost;
+        System.out.println( miles + " количество миль за билет");
+    }
+}
